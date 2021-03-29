bae4e48e903241d1c448ce461f3d07ab9f0f0b96
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-29 17:45:07 +0000

    reordered variables

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# imports math so program can use functions I have not created
-import math 
-
 # setting up variables
+import math
 pi = math.pi
 diameter = 0
 radius = 0
@@ -9,14 +7,14 @@ area = 0
 c = 0
 
 # user inputs diameter and their answer is converted to an integer
-print('Enter the diameter of a circle to find the area and circumference:')
+print ('Enter the diameter of a circle to find the area and circumference:')
 diameter = input()
-diameter = int(diameter)
+diameter = int (diameter)
 
-# the math 
+# the math
 radius = diameter / 2
 area = round (pi * (radius * radius), 1)
 c = round (2 * (pi * radius), 1)
 
-# final answer is printed 
-print('The area is ' , area, 'and the circumference is', c)
+# final answer is printed
+print ('The area is ' , area, 'and the circumference is', c)
