6f57a23347cf51fe36532441c02f8df140ee6b22
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-29 17:35:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# imports math so program can use functions I have not created
+import math 
+
+# setting up variables
+pi = math.pi
+diameter = 0
+radius = 0
+area = 0
+c = 0
+
+# user inputs diameter and their answer is converted to an integer
+print('Enter the diameter of a circle to find the area and circumference:')
+diameter = input()
+diameter = int(diameter)
+
+# the math 
+radius = diameter / 2
+area = round (pi * (radius * radius), 1)
+c = round (2 * (pi * radius), 1)
+
+# final answer is printed 
+print('The area is ' , area, 'and the circumference is', c)
